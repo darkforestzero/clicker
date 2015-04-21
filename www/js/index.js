feb4d12dcefbd5c25ee4baa9480a75eb9babd68c
Jonathan Rodgers <darkforestzer@gmail.com>
2015-04-20 22:01:41 -0700

    encapsulate variables in game

--- www/js/index.js
+++ www/js/index.js
@@ -1,27 +1,12 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    clicks: 0,
-    button: null,
-    counter: null,
+    // TODO: make everything use game.vars instead of this.vars
     // Application Constructor
     initialize: function () {
+        this.game = {
+            clicks: 0,
+            button: null,
+            counter: null
+        };
         this.bindEvents();
     },
     // Bind Event Listeners
@@ -42,14 +27,14 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
-        app.receivedEvent('deviceready');
+        this.receivedEvent('deviceready');
 
-        this.button = document.getElementById("theButton");
-        this.counter = document.getElementById("counter");
-        var that = this;
-        this.button.onclick = function () {
-            that.counter.innerHTML = 'Clicks: ' + (++that.clicks).toString();
-            console.log('bwaaa:' + that.clicks.toString());
+        var game = this.game;
+        game.button = document.getElementById("theButton");
+        game.counter = document.getElementById("counter");
+        game.button.onclick = function () {
+            game.counter.innerHTML = 'Clicks: ' + (++game.clicks).toString();
+            console.log('bwaaa:' + game.clicks.toString());
         };
     },
     // Update DOM on a Received Event
